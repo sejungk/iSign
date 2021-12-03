b74e4dead1d6b32c7673156b9bcd81e8b52134e7
Sejung <49921709+sejungk@users.noreply.github.com>
2021-12-02 23:47:13 -0500

    feat: created landing page component

---
+++ src/components/LandingPage.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+const LandingPage = () => {
+  return (
+    <div className="landing-page-wrapper">
+        <div className="landing-page-text-wrapper">
+          <h1>
+            A new way to learn American Sign Language
+          </h1>
+          <p>
+            An interactive learning platform designed to aid you in your ASL learning journey.
+          </p>
+          <div className="signup-bttn">Get Started</div>
+        </div>
+        <div>
+          <img src="https://drive.google.com/uc?export=view&id=1Qne_O096dVPtBE53wWv41D9ZNT-481wW" />
+        </div>
+    </div>
+  )
+}
+
+export default LandingPage
